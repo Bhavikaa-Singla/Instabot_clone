acc61e3bc76633dad139da233757bbcfc4da79c9
Bhavikaa-Singla <bhavikasingla99@gmail.com>
2017-07-26 03:06:25 +0530

    Added new functionality to send an email to the user who create the post informing them of a new comment on the post they created

--- myapp/views.py
+++ myapp/views.py
@@ -179,7 +179,7 @@ def like_view(request):
                 LikeModel.objects.create(post_id=post_id, user=user)
                 # like = LikeModel.objects.create(post_id=post_id, user=user)
                 # sg = sendgrid.SendGridAPIClient(apikey=(SENDGRID_API_KEY))
-                # from_email = Email("apooravsharma1997@gmail.com")
+                # from_email = Email("bhavikasingla99@gmail.com")
                 # to_email = Email(like.post.user.email)
                 # subject = "Welcome to Instagram Clone!!"
                 # content = Content("text/plain", "someone just liked your post. Go checkout!")
@@ -216,6 +216,16 @@ def comment_view(request):
                 review = "Negative Comment!"
             comment = CommentModel.objects.create(user=user, post_id=post_id, comment_text=comment_text, review=review)
             comment.save()
+            # sg = sendgrid.SendGridAPIClient(apikey=(SENDGRID_API_KEY))
+            # from_email = Email("bhavikasingla99@gmail.com")
+            # to_email = Email(comment.post.user.email)
+            # subject = "Welcome to Instagram Clone!!"
+            # content = Content("text/plain", "someone just commented on your post. Go check out!")
+            # mail = Mail(from_email, subject, to_email, content)
+            # response = sg.client.mail.send.post(request_body=mail.get())
+            # print(response.status_code)
+            # print(response.body)
+            # print(response.headers)
             return redirect('/feed/')
         else:
             return redirect('/feed/')
